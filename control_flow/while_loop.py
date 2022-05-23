ed44ac6e2f5d23bbe753309864b8065d2d0f97a7
Abhiram <abhiram.s4896@gmail.com>
2022-05-23 15:09:18 +0100

    Introduction to Control

--- control_flow/while_loop.py
+++ control_flow/while_loop.py
@@ -1,14 +1,27 @@
-x = 1
+# x = 1
+#
+# while x <= 100:
+#     print(f"{x}")
+#     x += 1
+#     if x % 3 == 0 and x % 5 != 0:
+#         print("Fizz!")
+#     if x % 5 == 0 and x % 3 != 0:
+#         print("Buzz!")
+#     if x % 5 == 0 and x % 3 == 0:
+#         print("Fizz Buzz!")
+#
+#
+# print("Fizz Buzz completed!")
 
-while x <= 100:
-    print(f"{x}")
-    x += 1
-    if x % 3 == 0 and x % 5 != 0:
-        print("Fizz!")
-    if x % 5 == 0 and x % 3 != 0:
-        print("Buzz!")
-    if x % 5 == 0 and x % 3 == 0:
-        print("Fizz Buzz!")
+keep_asking = True
+age_int = None
 
+while keep_asking:
+    age = input("What is your age?\n")
+    if age.isdigit():
+        age_int = int(age)
+        keep_asking = False
+    else:
+        print("Please enter a valid number in digits")
 
-print("Fizz Buzz completed!")
+print(f"Your age is {age_int}")
