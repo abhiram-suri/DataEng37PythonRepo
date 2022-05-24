05a134fd08556678beb0fd56f6ab4561d426c90a
Abhiram <abhiram.s4896@gmail.com>
2022-05-24 22:14:44 +0100

    Introduction to Control

--- control_flow/fizzbuzz_ame.py
+++ control_flow/fizzbuzz_ame.py
@@ -25,28 +25,28 @@
 #
 # print("Thank you for playing Fizz Buzz!")
 
-while True:
-    continue_input = "no"
-
-    if continue_input.lower() in ("no", "n"):
-        break
+# while True:
+#     continue_input = "no"
+#
+#     if continue_input.lower() in ("no", "n"):
+#         break
 
 play = input("Press f to play Fizz Buzz\n")
 while play == 'f':
     input_num = ''
     while not input_num.isdigit():
         input_num = input("What number would you like to play Fizz Buzz until?\n")
-for x in range(1, int(input_num) + 1):
-        if x % 3 == 0 and x % 5 != 0:
-            print("Fizz!")
-        elif x % 5 == 0 and x % 3 != 0:
-            print("Buzz!")
-        elif x % 5 == 0 and x % 3 == 0:
-            print("Fizz Buzz!")
-        else:
-            print(f"{x}")
+        for x in range(1, int(input_num) + 1):
+                if x % 3 == 0 and x % 5 != 0:
+                    print("Fizz!")
+                elif x % 5 == 0 and x % 3 != 0:
+                    print("Buzz!")
+                elif x % 5 == 0 and x % 3 == 0:
+                    print("Fizz Buzz!")
+                else:
+                    print(f"{x}")
 
-play = input("\nPress p to play again, or q to quit: ")
+        play = input("\nPress p to play again, or q to quit: ")
 
-print("Fizz Buzz Completed")
+        print("Fizz Buzz Completed")
 
