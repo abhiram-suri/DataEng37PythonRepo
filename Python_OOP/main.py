42002b2e39cb886bbe7e7858f04c13affaa01043
Abhiram <abhiram.s4896@gmail.com>
2022-05-24 09:30:35 +0100

    Introduction to Control

---
+++ Python_OOP/main.py
@@ -0,0 +1,4 @@
+import abstraction_customer
+
+cust = abstraction_customer.Customer('Jyoti', 'Suresh')
+cust.print()
--- control_flow/fizzbuzz_ame.py
+++ control_flow/fizzbuzz_ame.py
@@ -1,15 +1,47 @@
-x = 0
+#
+# keep_asking = True
+# input_a = none
 
-while x < 100:
-    x += 1
-    if x % 3 == 0 and x % 5 != 0:
-        print("Fizz!")
-    elif x % 5 == 0 and x % 3 != 0:
-        print("Buzz!")
-    elif x % 5 == 0 and x % 3 == 0:
-        print("Fizz Buzz!")
-    else:
-        print(f"{x}")
+# while keep_asking:
+# response = input("Would you like to play Fizz Buzz?\n")
+#(
+# if
 
 
-print("Fizz Buzz completed!")
+# x = 0
+#
+# input_num = input("What number would you like to play Fizz Buzz until?\n")
+#
+# while x < int(input_num):
+#     x += 1
+#     if x % 3 == 0 and x % 5 != 0:
+#         print("Fizz!")
+#     elif x % 5 == 0 and x % 3 != 0:
+#         print("Buzz!")
+#     elif x % 5 == 0 and x % 3 == 0:
+#         print("Fizz Buzz!")
+#     else:
+#         print(f"{x}")
+#
+# print("Thank you for playing Fizz Buzz!")
+
+keep_asking = True
+response = None
+
+while keep_asking:
+    play_again = input("Would you like to play?\n")
+    if play_again : "Yes"
+    x = 0
+    input_num = input("What number would you like to play Fizz Buzz until?\n")
+    while x < int(input_num):
+            x += 1
+            if x % 3 == 0 and x % 5 != 0:
+                print("Fizz!")
+            elif x % 5 == 0 and x % 3 != 0:
+                print("Buzz!")
+            elif x % 5 == 0 and x % 3 == 0:
+                print("Fizz Buzz!")
+            else: print(f"{x}")
+            keep_asking = False
+    else: play_again = "No"
+    print("Fizz Buzz Completed")
