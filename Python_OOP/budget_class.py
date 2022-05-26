b976551618ddd94d0259df3f3911d3a980a6fa11
Abhiram <abhiram.s4896@gmail.com>
2022-05-26 09:02:22 +0100

    Python OOP

--- Python_OOP/budget_class.py
+++ Python_OOP/budget_class.py
@@ -20,6 +20,8 @@ class Budget:
         if transfer_funds > 0 and self.amount > transfer_funds:
             self.withdraw(transfer_funds)
             category.deposit(transfer_funds)
+        else:
+            print("Insufficient balance for transfer")
 
     def balance(self):
         return self.amount
@@ -48,6 +50,7 @@ cloth_budget.deposit(50)
 food_budget.deposit(60)
 cloth_budget.balance()
 cloth_budget.transfer(20, food_budget)
+cloth_budget.transfer(100, food_budget)
 
 print("The current balance for food is £" + str(food_budget.amount))
 print("The current balance for clothes is £" + str(cloth_budget.amount))
