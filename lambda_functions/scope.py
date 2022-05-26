3ec0a1047f021c7485d6a32d03206b5438c06947
Abhiram <abhiram.s4896@gmail.com>
2022-05-26 14:08:07 +0100

    Lambda Functions

--- lambda_functions/scope.py
+++ lambda_functions/scope.py
@@ -3,5 +3,16 @@
 # Local
 # Enclosed
 # Global
+
+glob = "This is in the global scope"
+
+def local_function():
+    def inner_function():
+        print(__name__)
+    inner_function()
+
+def type(x):
+    return "I dunno, probably a string"
+
 # Built-In
 
