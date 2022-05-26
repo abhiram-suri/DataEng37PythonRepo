4ed8e8e92b80f641b8a56b2eb03e6af41f218df6
Abhiram <abhiram.s4896@gmail.com>
2022-05-26 08:55:38 +0100

    Python OOP

--- Python_OOP/budget_class.py
+++ Python_OOP/budget_class.py
@@ -1,2 +1,55 @@
 # Create a Budget class that can keep track of different budget categories like food, clothing, and entertainment.
 # These should allow for depositing and withdrawing funds from each category, as well computing category balances and transferring balance amounts between categories
+
+class Budget:
+    def __init__(self, category):
+        self.category = category
+        self.amount = 0
+
+
+    def deposit(self, deposit_add):
+        self.amount = self.amount + deposit_add
+
+    def withdraw(self, withdraw_subtract):
+        if withdraw_subtract > 0 and self.amount > withdraw_subtract:
+            self.amount -= withdraw_subtract
+        else:
+            print("Insufficient balance for withdrawal")
+
+    def transfer(self, transfer_funds, category):
+        if transfer_funds > 0 and self.amount > transfer_funds:
+            self.withdraw(transfer_funds)
+            category.deposit(transfer_funds)
+
+    def balance(self):
+        return self.amount
+
+
+class Food(Budget):
+    def __init__(self):
+        super().__init__("Food")
+
+
+class Entertainment(Budget):
+    def __init__(self):
+        super().__init__("Entertainment")
+
+class Clothing(Budget):
+    def __init__(self):
+        super().__init__("Clothing")
+
+
+food_budget = Food()
+ent_budget = Entertainment()
+cloth_budget = Clothing()
+
+cloth_budget.withdraw(40)
+cloth_budget.deposit(50)
+food_budget.deposit(60)
+cloth_budget.balance()
+cloth_budget.transfer(20, food_budget)
+
+print("The current balance for food is £" + str(food_budget.amount))
+print("The current balance for clothes is £" + str(cloth_budget.amount))
+print("The current balance for entertainment is £" + str(ent_budget.amount))
+
