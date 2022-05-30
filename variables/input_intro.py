2fb8b8cc7268ff31458a4c022d7931e18cec7366
Abhiram <abhiram.s4896@gmail.com>
2022-05-30 08:56:48 +0100

    Introduction to Variables

--- variables/input_intro.py
+++ variables/input_intro.py
@@ -3,3 +3,4 @@ team_manager = input("Who is your manager?\n")
 trophies_number = input("How many trophies have you won this season?\n")
 
 print(f"My favourite team is {team_name}, our manager is {team_manager} and we won {trophies_number} trophies this season")
+
