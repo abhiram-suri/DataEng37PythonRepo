69083851ba59e7b825fadfdb5bdfe1aca425c546
Abhiram <abhiram.s4896@gmail.com>
2022-06-08 22:45:50 +0100

    Pymongo

---
+++ pymongo/pymongo_demo.py
@@ -0,0 +1,31 @@
+import pymongo
+
+client = pymongo.MongoClient()
+print(client)
+
+db = client['starwars']
+print(db)
+
+luke = db.characters.find_one({"name": "Luke Skywalker"})
+print(luke)
+print(luke['height'])
+
+# droids = db.characters.find(
+#     {"species.name": "Droid"}
+# )
+# for droid in droids:
+#     print(droid['name'])
+
+darth_vader = db.characters.find_one({"name": "Darth Vader"})
+print(darth_vader['name'], ",",  darth_vader['height'])
+
+yellow_eyes = db.characters.find({"eye_color": "yellow"})
+print(list(yellow['name'] for yellow in yellow_eyes))
+
+male_char = db.characters.find({"gender": "male"}).limit(3)
+print(list(male['name'] for male in male_char))
+
+alderaan_human = db.characters.find({"species.name": "Human", "homeworld.name": "Alderaan"})
+print(list(ald['name'] for ald in alderaan_human))
+
+
